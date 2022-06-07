eafae4422ae6f02985544f0b78ec37435eb65038
david doria <dor15004@byui.edu>
2022-06-07 17:23:55 -0600

    Added player idle animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-
 import 'dart:async';
+import 'dart:ui';
 
+import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
 
 import 'package:flutter/material.dart';
-
-import 'package:ninja_jump/components/player.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,10 +19,18 @@ Future<void> main() async {
 
 /// This class encapulates the whole game.
 class NinjaJumpGame extends FlameGame {
-  final Player _player = Player();
-
   @override
   Future<void> onLoad() async {
-    add(_player);
+    final idleSpriteSheetImage = await images.load('Idle.png');
+    final idleSpriteSheet =
+        SpriteSheet(image: idleSpriteSheetImage, srcSize: Vector2.all(200));
+    final idleAnimation = idleSpriteSheet.createAnimation(row: 0, stepTime: .15);
+
+    final playerComponent = SpriteAnimationComponent(
+      animation: idleAnimation,
+      size: Vector2(200, 200),
+    );
+
+    add(playerComponent);
   }
 }
